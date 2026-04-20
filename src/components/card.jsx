4cedf3566dede13b72506d490e72e9147721102f
Amit Kumar <www.yadavanshiamit@gmail.com>
2026-04-20 23:29:21 +0530

    second commmit

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -3,7 +3,7 @@
 export default function ProfileCard(user){
     console.log(user);
     return(
-       <div className="border border rounded m-2 h-60 w-55">
+       <div className="border border-rounded m-2 h-60 w-55">
          <img src={user.user.avatar_url} className="h-45 w-60" alt="" />
          <h2 className="text-center pt-2">{user.user.login}</h2>
        </div>
